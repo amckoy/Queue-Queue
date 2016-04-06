c891ec7d02ffd866fa4e2529efef2545ac41b6e7
eccentricayman <aahmed3@stuy.edu>
2016-04-06 17:03:19 -0400

    REPLACE LLNODE WITH DLLNODE

--- Deque.java
+++ Deque.java
@@ -1,7 +1,28 @@
 public class Deque<T>{
-    private LLNode<T> _front, _end;
 
-    public Deque(){
-	_front = _end = null;
+    private LLNode<T> front, end;
+
+    public Deque() {
+		front = end = new LLNode<T>(null, null, null);
+		front.setNext(end);
+		end.setPrev(front);
     }
+
+    public boolean add(T e) {
+        LLNode<T> temp = new LLNode(e, end, null);
+        temp.setPrev(end.getPrev());
+        end.getPrev().setNext(temp);
+        return true;
+    }
+
+    public void addFirst(T e) {
+    	LLNode<T> temp = new LLNode(e, front.getNext(), front);
+    	front.setNext(temp);
+    }
+
+    public void addLast(T e) {
+    	LLNode<T> temp = new LLNode(e, end, end.getPrev());
+  		
+    }
+
 }
