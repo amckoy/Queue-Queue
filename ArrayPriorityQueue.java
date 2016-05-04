06978e83bbcc0e0501a5b8c4eb7ac1a647efd1f2
eccentricayman <aahmed3@stuy.edu>
2016-05-04 11:48:27 -0400

    do the damn main method

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -10,9 +10,47 @@ import java.util.ArrayList;
 public class ArrayPriorityQueue implements PriorityQueue {
 
     private ArrayList<Comparable> queue;
-    
-    public ArrayPriorityQueue() {
         
+    public ArrayPriorityQueue() {
+        queue = new ArrayList<Comparable>();
+    }
+
+    public void add(Object x) {
+        queue.add(x);
+    }
+
+    public boolean isEmpty() {
+        return queue.size() == 0;
+    }
+
+    public Object peekMin() {
+        //IM SORRY FOR DA TYPECASTING :(
+        Object temp = (Comparable)(queue.get(0));
+        for (int i = 1 ; i < queue.size() ; i++) {
+            if (temp.compareTo(queue.get(i) > 0)) {
+                //IM SORRY PT. 2
+                temp = (Comparable)(queue.get(i));
+            }
+        }
+        return temp;
+    }
+
+    public Object removeMin() {
+        int index = 0;
+        Object temp = (Comparable)(queue.get(0));
+        for (int i = 1 ; i < queue.size() ; i++) {
+            if (temp.compareTo(queue.get(i) > 0)) {
+                //IM SORRY PT. 2
+                temp = (Comparable)(queue.get(i));
+                index = i;
+            }
+        }
+        return queue.remove(index);
+    }
+
+    public static void main(String[] args) {
+        ArrayPriorityQueue QUEUEQUEUE = new ArrayPriorityQueue();
+        //all anton's problem now
     }
     
 }
